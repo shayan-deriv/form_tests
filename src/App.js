f9b0785965a182bb647e29aacac3a26863ddf566
maryiafrantsava-binary <103181650+maryiafrantsava-binary@users.noreply.github.com>
2022-12-22 15:07:02 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import ValidationInReact from "./ValidationInReact";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Reactjs validation</h1>
+      <ValidationInReact />
+    </div>
+  );
+}
---
+++ src/__tests__/Tasks.test.js
@@ -0,0 +1,41 @@
+describe("Tasks", () => {
+  it("Select the option and check that option 'male' was selected", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the user has filled the form except of gender select", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the field 'First Name' was focused and filled with min 2 letters", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the button submit was clicked", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the form has a class", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the labels of the form have a content", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the user can open a list of the gender and doesn't choose anything", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the user can't to click a submit until fields will be correct filled", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that the field 'Email' should have correct validation", () => {
+    expect(true).toBe(true);
+  });
+
+  it("Check that all fields on the first render should be empty", () => {
+    expect(true).toBe(true);
+  });
+});
